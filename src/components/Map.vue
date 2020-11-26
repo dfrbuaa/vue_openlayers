d5a3e4582429db23d72e78f398ba3a128ce055a2
dfrbuaa <1162964677@qq.com>
2020-11-26 20:44:09 +0800

    多点标记1.0

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -12,28 +12,32 @@ import { OSM, TileArcGISRest, XYZ } from 'ol/source';
 import { Vector as SourceVec } from 'ol/source'
 import { Feature } from 'ol'
 import { Point } from 'ol/geom'
-import { Style, Icon } from 'ol/style'
+import { Style, Icon, Text } from 'ol/style'
 import { Vector as LayerVec } from 'ol/layer'
-
+import airports from '../assets/airports.json'
 export default {
   name: 'OlMap',
+  data () {
+    // var airports = JSON.parse(airports)
+    return {
+      airports: airports,
+      lnglats: [],
+      lnglats_ys: [],
+      lnglats_ty: [],
+      lnglats_tyzs: [],
+
+    }
+  },
   mounted () {
+    // console.log(airports)
     this.vectorTileLayer()
+
   },
 
   methods: {
     vectorTileLayer () {
       //渲染地图
       var layers = [
-        // new TileLayer({
-        //   source: new OSM()
-        // }),
-        // new TileLayer({
-        //   extent: [-13884991, 2870341, -7455066, 6338219],
-        //   source: new TileArcGISRest({
-        //     url: 'https://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Specialty/ESRI_StateCityHighway_USA/MapServer'
-        //   })
-        // }),
         new TileLayer({
           source: new XYZ({
             url: 'http://webst0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=7&x={x}&y={y}&z={z}'
@@ -51,41 +55,96 @@ export default {
           zoom: 5
         })
       })
-      var lnglats = [
-        [116.963, 36.623],
-        [120.980, 36.620]
 
-      ]
+      for (var j in this.airports) {
+
+        if (this.airports[j].demo == "1") {
+          this.lnglats_ys.push([this.airports[j].lon, this.airports[j].lat])
+        }
+        if (this.airports[j].demo == "2") {
+          this.lnglats_ty.push([this.airports[j].lon, this.airports[j].lat])
+        }
+        if (this.airports[j].demo == "3") {
+          this.lnglats_tyzs.push([this.airports[j].lon, this.airports[j].lat])
+        }
+      }
+
 
       //创建图标特性
-      var iconFeature = new Array()
-      for (var i = 0; i < lnglats.length; i++) {
-        iconFeature.push(new Feature({
-          geometry: new Point(lnglats[i])
+      var iconFeature1 = new Array()
+      for (var i = 0; i < this.lnglats_ys.length; i++) {
+        iconFeature1.push(new Feature({
+          geometry: new Point(this.lnglats_ys[i])
+        }))
+      }
+      var iconFeature2 = new Array()
+      for (var i = 0; i < this.lnglats_ty.length; i++) {
+        iconFeature2.push(new Feature({
+          geometry: new Point(this.lnglats_ty[i])
+        }))
+      }
+      var iconFeature3 = new Array()
+      for (var i = 0; i < this.lnglats_tyzs.length; i++) {
+        iconFeature3.push(new Feature({
+          geometry: new Point(this.lnglats_tyzs[i])
         }))
       }
       // 创建矢量容器
-      var vectorSource = new SourceVec({
-        features: iconFeature
+      var vectorSource1 = new SourceVec({
+        features: iconFeature1
       })
-      console.log(iconFeature)
+      var vectorSource2 = new SourceVec({
+        features: iconFeature2
+      })
+      var vectorSource3 = new SourceVec({
+        features: iconFeature3
+      })
+
       //创建图标样式
-      var iconStyle = new Style({
+      var iconStyle1 = new Style({
         image: new Icon({
           opacity: 0.75,
-          src: "https://fplcaac.com/img/icon-ys.png"
-          // src: "https://fplcaac.com/img/icon-tyzs.png"
+          src: "https://fplcaac.com/img/icon-ys.png",
+
+        }),
+        text: new Text({
+          text: 'aaa'
+        })
+      })
+      var iconStyle2 = new Style({
+        image: new Icon({
+          opacity: 0.75,
+          src: "https://fplcaac.com/img/icon-tyzs.png"
           // src: "https://fplcaac.com/img/icon-ty.png"
           // src: "https://fplcaac.com/img/icon_zdz.png"
+          // src: "https://fplcaac.com/img/icon_jcbw.png"
+        })
+      })
+      var iconStyle3 = new Style({
+        image: new Icon({
+          opacity: 0.75,
+          src: "https://fplcaac.com/img/icon-ty.png"
+          // src: "https://fplcaac.com/img/icon_zdz.png"
+          // src: "https://fplcaac.com/img/icon_jcbw.png"
         })
       })
       //创建矢量层
-      var vectorLayer = new LayerVec({
-        source: vectorSource,
-        style: iconStyle
-      });
+      var vectorLayer1 = new LayerVec({
+        source: vectorSource1,
+        style: iconStyle1
+      })
+      var vectorLayer2 = new LayerVec({
+        source: vectorSource2,
+        style: iconStyle2
+      })
+      var vectorLayer3 = new LayerVec({
+        source: vectorSource3,
+        style: iconStyle3
+      })
       //添加进map
-      map.addLayer(vectorLayer);
+      map.addLayer(vectorLayer1)
+      map.addLayer(vectorLayer2)
+      map.addLayer(vectorLayer3)
     }
 
 
